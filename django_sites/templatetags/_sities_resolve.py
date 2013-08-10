f2aecf968edc95fdab3ff47218e279c487464684
Andrey Antukh <niwi@niwi.be>
2013-08-10 17:21:35 +0200

    Fix invalid templatetag function register.

--- django_sites/templatetags/_sities_resolve.py
+++ django_sites/templatetags/_sities_resolve.py
@@ -5,6 +5,6 @@ from .. import utils
 try:
     from django_jinja.base import Library
     jinja_register = Library()
-    jinja_register.global_function("sites_resolve", utils.resolve)
+    jinja_register.global_function("sites_reverse", utils.reverse)
 except ImportError:
     pass
