b6b4b49e14eefb76b0a7479bcfbf2bc048dcd63d
Andrey Antukh <niwi@niwi.be>
2014-05-26 15:26:07 +0200

    Add missing import on __init__.py file.

--- django_sites/__init__.py
+++ django_sites/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .base import Site, get_current
+from .base import Site, get_current, get_by_id
 from .utils import reverse
 
-__all__ = ["reverse", "Site", "get_current"]
+__all__ = ["reverse", "Site", "get_current", "get_by_id"]
