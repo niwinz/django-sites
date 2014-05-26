8c892c216891cd76210042450d9f7c6928dc024e
Andrey Antukh <niwi@niwi.be>
2014-05-26 15:11:40 +0200

    Increment version to 0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if sys.version_info < (2, 7):
 
 setup(
     name = "django-sites",
-    version = "0.5",
+    version = "0.6",
     description = "Alternative implementation of django sites framework",
     long_description = "",
     keywords = "django, sites",
